65967abe1debb7296f7230facbdc080b325c9b6a
Cameron Pitcel <cpitcel@illinois.edu>
2017-02-28 17:17:49 -0600

    MySQL Read, Lecture 44
    How to Select from DB and display data

--- MySQL/login_create.php
+++ MySQL/login_create.php
@@ -5,20 +5,28 @@ if(isset($_POST['submit'])) {
     
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+    /*
     if($username && $password){
         echo 'Username: ' . $username . '<br>';
         echo 'Password: ' . $password . '<br>';
     } else {
         echo 'no data<br>';
     }
-    
+    */
     $connection = mysqli_connect('localhost', 'root', '', 'loginapp'); //machine, login user, password, database
     
     if($connection){
         echo 'connection established<br>';
     } else {
         die('connection DENIED<br>');
+    }
+    
+    $query = "INSERT INTO USERS(USERNAME, PASSWORD) ";
+    $query .= "VALUES ('$username', '$password')"; //quotes needed to show they are strings, good idea to use double quotes!
+    
+    $result = mysqli_query($connection, $query); //use the connection to execute the query
+    if(!$result){
+        die('Insert Query Failure' . mysqli_error());
     }
 }
 
@@ -34,7 +42,7 @@ if(isset($_POST['submit'])) {
 <body>
   <div class="container">
       <div class="col-sm-6">
-          <form action="login.php" method="post">
+          <form action="login_create.php" method="post">
               <div class="form-group">
                  <label for="username">Username</label>
                   <input type="text" name="username" class="form-control">
