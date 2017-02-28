9549dc39c5c4b4a93e82d1e8b60a0202027ff3ce
Cameron Pitcel <cpitcel@illinois.edu>
2017-02-28 10:46:36 -0600

    Lesson 33, Forms & Validation
    HTML form submission, validation.

--- form.php
+++ form.php
@@ -1,11 +1,26 @@
 <?php
     //$_POST //will pick up user data input from forms, as is defined
             //is a super global variable, as an array
+
+    $validuser = array('Mike', 'Chris', 'Cam', 'Bill', 'Matt', 'Shaq');
         
     if(isset($_POST['submit'])) { //must match name from submit (which is submit) 
         echo 'we got something!<br>';
         $user = $_POST['username'];
         $psw = $_POST['password'];
+        
+        if(strlen($user) < 4) {
+            echo 'username must be longer than 5<br>';
+        } elseif(strlen($user) > 10 ) {
+            echo 'username cannot be longer than 10<br>';
+        }
+        
+        if(!in_array($user, $validuser)){
+            echo 'Invalid user! You do not have access!<br>';
+        } else {
+            echo 'Welcome ' . $user . '<br>';
+        }
+        
         echo 'username: ' . $user . '<br>';
         echo 'password: ' . $psw . '<br>';
     }
