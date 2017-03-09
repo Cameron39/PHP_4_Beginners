04d6225dd025dfe3b2405941f074d7c424c71c3f
Cameron Pitcel <cpitcel@illinois.edu>
2017-03-09 17:16:57 -0600

    Empty method of a class, lecture 67
    Empty method inside a class, check if method exists in a class, lecture
    67

--- class_car.php
+++ class_car.php
@@ -2,14 +2,25 @@
 
 class Car {
     
-    
+    function MoveWheels() {
+        echo "Wheels Moved";
+        
+    }
     
 }
 
+/*
 if(class_exists("Car")) {
     echo "Exists";
 } else {
     echo "DNE";
 }
+*/
+if(method_exists("Car", "MoveWheels")) {
+    echo "Exists";
+} else {
+    echo "DNE";
+}
+
 
 ?>
