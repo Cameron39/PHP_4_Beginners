0287b8909e550d6cee1640d5307db827a9efb1d2
Cameron Pitcel <cpitcel@illinois.edu>
2017-03-09 16:31:26 -0600

    POST example, lecture 59
    Post example, not final

---
+++ post.php
@@ -0,0 +1,20 @@
+<?php
+echo $_POST["name"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>POST Example</title>
+</head>
+<body>
+   
+<form action="post.php" method="post">
+    <input type="text" name='name'>
+    <input type="submit">
+    
+    
+</form>
+    
+</body>
+</html>
