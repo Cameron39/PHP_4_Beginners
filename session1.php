c3d06f55f7bfdea1a2afcab76d4627bdad7cae5b
Cameron Pitcel <cpitcel@illinois.edu>
2017-03-09 16:55:55 -0600

    Sessions Set and Get example, lecture 63
    How to set data in the session and get it. Lecture 63

---
+++ session1.php
@@ -0,0 +1,22 @@
+
+<?php session_start();
+    $_SESSION['greeting'] = "HELLO ALL";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Session Example1</title>
+</head>
+<body>
+   
+   <?php
+    
+    
+    
+    ?>
+    
+</body>
+</html>
---
+++ session2.php
@@ -0,0 +1,22 @@
+
+<?php session_start();
+    echo $_SESSION['greeting'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Session Example1</title>
+</head>
+<body>
+   
+   <?php
+    
+    
+    
+    ?>
+    
+</body>
+</html>
