ed818d89e1bca0d6e78593fab4058778a2ff63af
Cameron Pitcel <cpitcel@illinois.edu>
2017-03-10 15:17:23 -0600

    Read from a file, lecture 77
    How to read from a file. Use filesize($file) to read everything from
    the file, lecture 77

--- files.php
+++ files.php
@@ -9,14 +9,27 @@
    <?php
     $file = "example.txt";
     
+    //writing from file
     if($handle = fopen($file, 'w')) { //w for write
-        echo 'file opened';
         fwrite($handle, 'Woot writing the file!');
     
     } else {
-        echo 'could not open file';
+        echo 'could not write file';
     }
     fclose($handle);
+    
+    //Reading from file
+    if($handle = fopen($file, 'r')) { //r for write
+        $content = fread($handle, 22); //second var is bytes, # of char
+        //use filesize($file) instead of number to get whole file
+        echo $content;
+    } else {
+        echo 'could not read file';
+    }
+    fclose($handle);
+    
+    
+    
     ?>
     
 </body>
