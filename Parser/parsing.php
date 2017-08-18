d7f31a43a9dcdf15d4ea3eeaf9e39d82d3afb91b
Cameron Pitcel <cpitcel@illinois.edu>
2017-08-18 10:50:36 -0500

    Working
    Working, get data from database using query.

--- Parser/parsing.php
+++ Parser/parsing.php
@@ -6,28 +6,21 @@
 //  $html = str_get_html(file_get_contents('http://www.google.com'));
 //echo out pure html: echo htmlspecialchars($html);
 include "getGuild.php";
-include "char.php";
 
-//require('simple_html_dom.php');
 $urls = array();
 $charAll = array();
 $master = array();
 $c = ",";
 $t = "<tab>";
+$q = "','";
 $servername = 'localhost';
 $username = 'root';
 $password = 'droid4swgoh';
 
 $urls = getUserUrls();
 
-//array_push($urls, 'https://swgoh.gg/u/darkness39/collection/');
-//array_push($urls, 'https://swgoh.gg/u/bigjohnnyk/collection/');
-/*
-Thoghts: user -> characters[char]
-Database?
--user | char | star | level | gear?
--need to create Database, create table;
-*/
+//$urls['darkness39'] = '/u/darkness39/'; //For testing 
+//$urls['bigjohnnyk'] = '/u/bigjohnnyk/';
 
 $conn = new mysqli($servername, $username, $password);
 
@@ -37,7 +30,7 @@ if ($conn->connect_error) {
     echo 'DB is okay<br>';
 }
 
-$sql = "CREATE DATABASE IF NOT EXISTS SW";
+$sql = 'CREATE DATABASE IF NOT EXISTS SW';
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully<br>";
 } else {
@@ -46,32 +39,33 @@ if ($conn->query($sql) === TRUE) {
 
 mysqli_select_db($conn,"SW");
 
-$sql = 'CREATE TABLE IF NOT EXISTS toons (
+$sql = 'DROP TABLE IF EXISTS TOONS';
+if ($conn->query($sql) === TRUE) {
+    echo 'Table Dropped<br>';
+} else {
+    die("Error truncating table: " . $conn->error);
+}
+
+$sql = 'CREATE TABLE TOONS (
         id INT NOT NULL AUTO_INCREMENT,
         PRIMARY KEY(id),
-        USER    char(100) NOT NULL,
-        TOON    char(100) NOT NULL,
+        USER    VARCHAR(100) NOT NULL,
+        TOON    VARCHAR(100) NOT NULL,
         STAR    INT,
+        VALID   CHAR(1),
         LEVEL   INT,
-        GEAR    char(5))';
+        GEAR    CHAR(5))';
 if ($conn->query($sql) === TRUE) {
     echo "Table Created<br>";
 } else {
     die("Error creating table: " . $conn->error);
 }
 
-
-
-// ...some PHP code for database "test"...
-
-mysqli_close($conn);
-
-exit();
 foreach ($urls as $user => $profile) {
 
     $url = 'https://swgoh.gg' . $profile . 'collection/';
     //echo $url . '<br>';
-
+    sleep(0.1);
     $html = file_get_html($url);
 
     $items = $html->find('div[class=col-xs-6 col-sm-3 col-md-3 col-lg-2]');
@@ -81,29 +75,53 @@ foreach ($urls as $user => $profile) {
         $level = getLevel($item->children(0)->children(0)->children(0));
         $gear = getGear($item->children(0)->children(0)->children(0));//->children(10));
         $stars = getStars($item->children(0)->children(0)->children(0));
-
-        echo $name . $c . $stars . $c . $level . $c . $gear . "<br>";
-        //echo $name . $t . $stars . "<br>";
+        if ($stars > 3) {
+            $valid = 'X';
+        } else {
+            $valid = 'N';
+        }
+        
+        $sql = "INSERT INTO TOONS (USER, TOON, STAR, VALID, LEVEL, GEAR) " .
+            "VALUES ('" . $user . $q . $name . $q . $stars . $q . $valid . 
+            $q . $level . $q . $gear . "')";
+        //echo $sql . '<br>';
+        
+        if ($conn->query($sql) === TRUE) {
+            //echo $name . $c . '<br>';
+        } else {
+            die ('Error Inserting: ' . $conn->error . '<br>SQL: ' . $sql);
+        }
+        //echo $name . $c . $stars . $c . $level . $c . $gear . $c . $valid . "<br>";
     }
-    //echo htmlspecialchars($items[0]->children(0)->children(0)->children(0));
-    //echo "<br><br>";
 
-    echo "<br><br>";
+    echo 'User: ' . $user . ' -> done<br>';
+    ob_flush();
+    flush();
+    //sleep(1);
+    //echo "<br><br>";
 }
 
+echo '<br>DONE!';
+
+mysqli_close($conn);
+
 function getName($html){
-    $name = "NA";
+
     //echo htmlspecialchars($html) . "<br>";
     if (($html->find('img[class=char-portrait-full-img]'))) {
-        $name = $html->children(0)->alt;
+        $tname = $html->children(0)->alt;
         //echo htmlspecialchars($html->children(0)->alt);
     }
     elseif (($html->find('img[class=char-portrait-img]'))) {
-        $name = $html->children(0)->children(0)->alt;
+        $tname = $html->children(0)->children(0)->alt;
         //echo htmlspecialchars($html->children(0)->children(0)->alt);
+    } else {
+        $tname = 'NA';
     }
     //echo htmlspecialchars($html);
-    return $name;
+    //$name = str_replace('&quot;',' ', $name); //seems to slow it down?
+    //$name = str_replace('&#39;',' ', $name);
+    return $tname;
 }
 
 function getLevel($html){
@@ -119,6 +137,53 @@ function getGear($html){
     if ($html !== NULL && ($html->find('div[class=char-portrait-full-gear-level]'))) {
         $gear = $html->children(10)->plaintext;
         //echo htmlspecialchars($html->children(10)->plaintext) . "<br>";
+    }
+    
+    switch ($gear) {
+        case 'I' : 
+            $gear = '1'; 
+            break;
+        case 'II' :
+            $gear = '2';
+            break;
+        case 'III' :
+            $gear = '3';
+            break;
+        case 'IV' :
+            $gear = '4';
+            break;
+        case 'V' :
+            $gear = '5';
+            break;
+        case 'VI' :
+            $gear = '6';
+            break;
+        case 'VII' :
+            $gear = '7';
+            break;
+        case 'VIII' :
+            $gear = '8';
+            break;
+        case 'IX' :
+            $gear = '9';
+            break;
+        case 'X' :
+            $gear = '10';
+            break;
+        case 'XI' :
+            $gear = '11';
+            break;
+        case 'XI' :
+            $gear = '12';
+            break;
+        case 'XII' :
+            $gear = '13';
+            break;
+        case 'XIII' :
+            $gear = '14';
+            break;
+        default :
+            $gear = '0';        
     }
     return $gear;
 }
